77da34e6adab49c9bef5bd3290475dae1cd8d4cd
Ivan Khlopyanik <berserk8@yandex.ru>
2012-11-01 22:25:55 +0300

    Helpers

--- cpp-cgdk/MyStrategy.cpp
+++ cpp-cgdk/MyStrategy.cpp
@@ -2,66 +2,213 @@
 
 #define _USE_MATH_DEFINES
 #include <cmath>
-#include <vector>
+#include "iostream"
 
 using namespace model;
-using namespace std;
 
 const double MIN_ANGLE = M_PI / 6.0; // угол в 30 градусов
 
 void MyStrategy::Move(Tank self, World world, model::Move& move) {
-    vector<Tank> all_tanks = world.tanks();                     // получим список всех танков
-    double min_angle_to_enemy = 1E20;
-    size_t selected_tank = all_tanks.size();
-    for(size_t i = 0; i < all_tanks.size(); ++i) {              // перебираем танк из списка
-        Tank tank = all_tanks[i];
-        if (!tank.teammate()) {                                 // в свои танки стрелять не будем :) 
-            double angle_to_enemy = fabs(self.GetTurretAngleTo(tank)); // найдем модуль угла от башни до танка
-            if (angle_to_enemy < min_angle_to_enemy) {          // выберем минимум
-                min_angle_to_enemy = angle_to_enemy;
-                selected_tank = i;
-            }
-        }
-    }
+    vector<Tank> allTanks = world.tanks();                     
+    double minAngleToEnemy = 1E20;
+    int selectedTank = GetNearestTankByAngle(self, allTanks, minAngleToEnemy);
 
-    if (selected_tank != all_tanks.size()) {
-        double angle_to_enemy = self.GetTurretAngleTo(all_tanks[selected_tank]); // найдем угол от башни до танка
+    if (selectedTank != -1) {
+        double angleToEnemy = minAngleToEnemy;//self.GetTurretAngleTo(allTanks[selectedTank]); // найдем угол от башни до танка
 
-        if (angle_to_enemy > MIN_ANGLE) {         // если угол сильно положительный
+        if (angleToEnemy > MIN_ANGLE) 
+		{         // если угол сильно положительный
             move.set_turret_turn(1.0);              
-        } else if (angle_to_enemy < -MIN_ANGLE) {  // если угол сильно отрицательный
+        } 
+		else if (angleToEnemy < -MIN_ANGLE) 
+		{  // если угол сильно отрицательный
             move.set_turret_turn(-1.0);
-        } else {
+        } 
+		else 
+		{
             move.set_fire_type(PREMIUM_PREFERRED); // если угол невелик, то выстрелим
         }
     }
 	
-	vector<Bonus> all_bonuses = world.bonuses();             // получим список всех бонусов
-    double min_dist_to_bonus = 1E20;
-    size_t selected_bonus = all_bonuses.size();
-    for(size_t i = 0; i < all_bonuses.size(); ++i) {         // перебираем бонус из списка
-       Bonus bonus = all_bonuses[i];
-       double dist_to_bonus = self.GetDistanceTo(bonus);    // найдем расстояние до бонуса
-       if (dist_to_bonus < min_dist_to_bonus) {             // найдем ближайший
-         min_dist_to_bonus = dist_to_bonus;
-         selected_bonus = i;
+	vector<Bonus> allBonuses = world.bonuses();             // получим список всех бонусов
+    double minDistToBonus = 1E20;
+    int selectedBonus = GetNearestBonus(self, allBonuses);
+	cout << selectedBonus;
+
+    if (selectedBonus != -1) {
+       double angleToBonus = self.GetAngleTo(allBonuses[selectedBonus]); // найдем угол до бонуса
+
+	   if ((angleToBonus < MIN_ANGLE && angleToBonus > 0) ||
+		   (angleToBonus > -MIN_ANGLE && angleToBonus < 0))
+	   {
+		   move.set_left_track_power(1.0);        
+           move.set_right_track_power(1.0); 
+	   }
+	   else if ((angleToBonus > M_PI - MIN_ANGLE && angleToBonus > 0) ||
+				(angleToBonus < -(M_PI - MIN_ANGLE)  && angleToBonus < 0))
+	   {
+			move.set_left_track_power(-1.0);         
+			move.set_right_track_power(-1.0); 
+	   }
+	   else if (angleToBonus > MIN_ANGLE) 
+	   {         
+			move.set_left_track_power(0.75);      
+			move.set_right_track_power(-1.0);       
+       } 
+	   else if (angleToBonus < -MIN_ANGLE) 
+	   {  
+			move.set_left_track_power(-1.0);         
+			move.set_right_track_power(0.75);    
+       } 
+	   else 
+	   {
+			move.set_left_track_power(1.0);         
+			move.set_right_track_power(1.0);       
        }
     }
+}
 
-    if (selected_bonus != all_bonuses.size()) {
-       double angle_to_bonus = self.GetAngleTo(all_bonuses[selected_bonus]); // найдем угол до бонуса
+//bonuses
+int MyStrategy::GetNearestBonus(Tank &self,vector<Bonus> &bonuses)
+{
+	double minDistToBonus = -1;
+	int selectedBonus = -1;
 
-       if (angle_to_bonus > MIN_ANGLE) {         // если угол сильно положительный,
-         move.set_left_track_power(0.75);      // то будем разворачиваться,
-         move.set_right_track_power(-1.0);        // поставив противоположные силы гусеницам.
-       } else if (angle_to_bonus < -MIN_ANGLE) {  // если угол сильно отрицательный,
-         move.set_left_track_power(-1.0);         // будем разворачиваться
-         move.set_right_track_power(0.75);     // в противоположную сторону.
-       } else {
-         move.set_left_track_power(1.0);         // если угол не больше 30 градусов
-         move.set_right_track_power(1.0);        // поедем максимально быстро вперед 
-       }
+	for(size_t i = 0; i < bonuses.size(); ++i)
+	{     
+		Bonus bonus = bonuses[i];
+		double distToBonus = self.GetDistanceTo(bonus);    
+		
+		if (distToBonus < minDistToBonus || minDistToBonus < 0) 
+		{            
+			minDistToBonus = minDistToBonus;
+			selectedBonus = i;
+		}
+	}
+
+	return selectedBonus;
+}
+
+int MyStrategy::GetNearestBonusByType(Tank &self, vector<Bonus> &bonuses, BonusType type)
+{
+	double minDistToBonus = -1;
+	int selectedBonus = -1;
+
+	for(size_t i = 0; i < bonuses.size(); ++i)
+	{     
+		Bonus bonus = bonuses[i];
+		double distToBonus = self.GetDistanceTo(bonus);    
+		
+		if ((distToBonus < minDistToBonus || minDistToBonus < 0) && bonus.type() == type) 
+		{            
+			minDistToBonus = minDistToBonus;
+			selectedBonus = i;
+		}
+	}
+
+	return selectedBonus;
+}
+
+//opponents
+int MyStrategy::GetNearestTankByAngle(Tank &self, vector<model::Tank> &tanks, double &angleToTarget)
+{
+	angleToTarget = 999;
+    int selectedTank = -1;
+
+    for(size_t i = 0; i < tanks.size(); ++i) 
+	{             
+        Tank tank = tanks[i];
+        bool isAlive = tank.crew_health() > 0 && tank.hull_durability() > 0;
+
+		if (!tank.teammate() && isAlive) 
+		{                                
+            double anglToEnemy = self.GetTurretAngleTo(tank); 
+            
+			if (fabs(anglToEnemy) < fabs(angleToTarget) || angleToTarget == 999) 
+			{          
+                angleToTarget = anglToEnemy;
+                selectedTank = i;
+            }
+        }
     }
+
+	return selectedTank;
+}
+
+int MyStrategy::GetNearestTankByDistance(Tank &self, vector<model::Tank> &tanks, double &distanceToTarget)
+{
+	distanceToTarget = -1;
+    int selectedTank = -1;
+
+    for(size_t i = 0; i < tanks.size(); ++i) 
+	{             
+        Tank tank = tanks[i];
+        bool isAlive = tank.crew_health() > 0 && tank.hull_durability() > 0;
+		
+		if (!tank.teammate() && isAlive) 
+		{                                
+            double distToEnemy = self.GetDistanceTo(tank);   
+
+			if (distToEnemy < distanceToTarget || distanceToTarget < 0) 
+			{          
+                distanceToTarget = distToEnemy;
+                selectedTank = i;
+            }
+        }
+    }
+
+	return selectedTank;
+}
+
+int MyStrategy::GetTankWithMinHP(vector<model::Tank> &tanks, int &targetHP )
+{
+	targetHP = -1;
+	int selectedTank = -1;
+	
+    for(size_t i = 0; i < tanks.size(); ++i) 
+	{             
+        Tank tank = tanks[i];
+        bool isAlive = tank.crew_health() > 0 && tank.hull_durability() > 0;
+		
+		if (!tank.teammate() && isAlive) 
+		{                                
+            int enemyHP = tank.crew_health();   
+
+			if (enemyHP < targetHP || targetHP < 0) 
+			{          
+                targetHP = enemyHP;
+                selectedTank = i;
+            }
+        }
+    }
+
+	return selectedTank;
+}
+
+int MyStrategy::GetTankWithMaxHP(vector<model::Tank> &tanks, int &targetHP )
+{
+
+	targetHP = -1;
+	int selectedTank = -1;
+	
+    for(size_t i = 0; i < tanks.size(); ++i) 
+	{             
+        Tank tank = tanks[i];
+        bool isAlive = tank.crew_health() > 0 && tank.hull_durability() > 0;
+		
+		if (!tank.teammate() && isAlive) 
+		{                                
+            int enemyHP = tank.crew_health();   
+
+			if (enemyHP > targetHP || targetHP < 0) 
+			{          
+                targetHP = enemyHP;
+                selectedTank = i;
+            }
+        }
+    }
+
+	return selectedTank;
 }
 
 TankType MyStrategy::SelectTank(int tank_index, int team_size) {
--- cpp-cgdk/MyStrategy.h
+++ cpp-cgdk/MyStrategy.h
@@ -4,11 +4,29 @@
 #define _MY_STRATEGY_H_
 
 #include "Strategy.h"
+#include <vector>
+
+using namespace std;
 
 class MyStrategy : public Strategy {
 public:
 	void Move(model::Tank self, model::World world, model::Move& move);
+
+	//bonuses
+	int GetNearestBonus(model::Tank &self, vector<model::Bonus> &bonuses);
+	int GetNearestBonusByType(model::Tank &self, vector<model::Bonus> &bonuses, model::BonusType type);
+
+	//opponents
+	int GetNearestTankByAngle(model::Tank &self, vector<model::Tank> &tanks, double &angleToTarget /*out*/);
+	int GetNearestTankByDistance(model::Tank &self, vector<model::Tank> &tanks, double &distanceToTarget /*out*/);
+	int GetTankWithMinHP(vector<model::Tank> &tanks, int &targetHP /*out*/);
+	int GetTankWithMaxHP(vector<model::Tank> &tanks, int &targetHP /*out*/);
+
 	model::TankType SelectTank(int tank_index, int team_size);
+
+private:
+	double currentSpeed; //pixels pre tick
+	
 };
 
 #endif
